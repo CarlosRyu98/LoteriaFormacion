953b3ade9345fed438c5eb002f0f408756c4f7a9
David Garcia <davidgarcia@MacBook-Pro-de-David.local>
2022-09-19 16:49:15 +0200

    euue

---
+++ src/main/scala/Inicio.scala
@@ -0,0 +1,49 @@
+package main.scala
+
+import org.apache.spark.sql.SparkSession
+
+object Inicio extends App{
+    println("hello World")
+  val variable1 = 1
+  val variable2 = 2
+    println("hello World")
+  println(variable2)
+  println(variable1)
+val spark = SparkSession.builder()
+  .getOrCreate()
+  val df = spark.read.table()
+  import spark.implicits._
+  println(s"variable1:$variable1, variable2:$variable2")
+
+  df.filter($"col1".gt())
+
+  /**
+   * hola aqui andamos
+   */
+  def hola
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  hola
+}
---
+++ src/main/scala/Inicio2.scala
@@ -0,0 +1,7 @@
+package main.scala
+
+object Inicio2 {
+  def main(args: Array[String]): Unit = {
+    println(s"args:${args.mkString("/").toString}")
+  }
+}
