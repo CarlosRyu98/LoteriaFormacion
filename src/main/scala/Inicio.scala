e7283a235ebeabefbff558fbf300c16feb5b08c3
Jaime Casado <jaime.casado@bosonit.com>
2022-09-22 11:36:51 +0200

    Eliminada dependencia del df en el metodo calcularCombinacion

--- src/main/scala/Inicio.scala
+++ src/main/scala/Inicio.scala
@@ -16,7 +16,9 @@ object Inicio extends App with sparkSession with LazyLogging {
   CombinarPares.combinarPares(lotoDF)
 
   val pruebaDF = lotoDF.select(Euromillon.NUM1, Euromillon.NUM2, Euromillon.NUM3, Euromillon.NUM4, Euromillon.NUM5)
-  val combinatedDF = Functions.addCombinatoryColumn(pruebaDF,pruebaDF.columns.length,1,0)
+  val columnsArray = Functions.calculateCombinationColumns(pruebaDF,pruebaDF.columns.length,1,0)
+  columnsArray.foreach(println)
+  val combinatedDF = lotoDF.select(columnsArray: _*)
   val numOfCombinations = Functions.contarCombinaciones(combinatedDF)
   numOfCombinations.show()
 }
--- src/main/scala/commons/Functions.scala
+++ src/main/scala/commons/Functions.scala
@@ -1,8 +1,7 @@
 package commons
 
-import model.Euromillon
-import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Column, DataFrame}
 
 object Functions {
   def countNumByColumn(column: String, df: DataFrame): DataFrame = {
@@ -13,47 +12,47 @@ object Functions {
     columns.reduce(_ union _).groupBy("num").sum("count").sort(desc("sum(count)"))
   }
 
-  def addCombinatoryColumn(df: DataFrame, originalDFSize: Int, actualPos: Int, initialPos: Int = 0): DataFrame = {
+  def calculateCombinationColumns(df: DataFrame, originalDFSize: Int, actualPos: Int, initialPos: Int = 0): Array[Column] = {
     val newColumnName = s"${initialPos + 1}_${actualPos + 1}"
+    val col1: String = df.columns(initialPos)
+    val col2: String = df.columns(actualPos)
+
     if (actualPos < originalDFSize - 1) {
-      addCombinatoryColumn(
-        calcularCombinacion(df, actualPos, initialPos, newColumnName),
+
+      calculateCombinationColumns(
+        df,
         originalDFSize,
-        actualPos = actualPos + 1,
-        initialPos = initialPos
-      )
+        actualPos + 1,
+        initialPos
+      ) :+ calcularCombinacion(col2, col1, newColumnName)
+
     } else if (initialPos < originalDFSize - 2) {
-      addCombinatoryColumn(
-        calcularCombinacion(df, actualPos, initialPos, newColumnName),
+
+      calculateCombinationColumns(
+        df,
         originalDFSize,
-        actualPos = initialPos + 2,
-        initialPos = initialPos + 1
-      )
-    } else calcularCombinacion(df, actualPos, initialPos, newColumnName)
+        initialPos + 2,
+        initialPos + 1
+      ) :+ calcularCombinacion(col2, col1, newColumnName)
+
+    } else Array(calcularCombinacion(col2, col1, newColumnName))
   }
 
 
-  private def calcularCombinacion(df: DataFrame, actualPos: Int, initialPos: Int, newColumnName: String) = {
-    df.withColumn(
-      newColumnName,
-      sort_array(
-        array(
-          df.columns(initialPos),
-          df.columns(actualPos)
-        )
+  private def calcularCombinacion(actualPos: String, initialPos: String, newColumnName: String) = {
+    sort_array(
+      array(
+        initialPos,
+        actualPos
       )
-    )
+    ).as(newColumnName)
   }
 
   def contarCombinaciones(combinadoDF: DataFrame): DataFrame = {
-    val auxDF = combinadoDF
-      .drop(Euromillon.NUM5, Euromillon.NUM4, Euromillon.NUM3, Euromillon.NUM2, Euromillon.NUM1)
-
-    auxDF
-      .withColumn("combinacionesTotales", explode(array(auxDF.columns.map( auxDF(_)): _*)))
+    combinadoDF
+      .withColumn("combinacionesTotales", explode(array(combinadoDF.columns.map(combinadoDF(_)): _*)))
       .groupBy("combinacionesTotales")
       .count()
       .orderBy(desc("count"))
   }
-
 }
