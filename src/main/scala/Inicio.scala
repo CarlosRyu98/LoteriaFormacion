995d2b36c9e2b07094c48a815d5bd71bb395c95e
Fran Lara <franciscojavier.lara@bosonit.com>
2022-09-22 14:09:35 +0200

    Conteo trios realizado

--- src/main/scala/Inicio.scala
+++ src/main/scala/Inicio.scala
@@ -15,16 +15,18 @@ object Inicio extends App with sparkSession with LazyLogging {
 
   CombinarPares.combinarPares(lotoDF)
 
+  val columns = Array(
+    Euromillon.NUM1,
+    Euromillon.NUM2,
+    Euromillon.NUM3,
+    Euromillon.NUM4,
+    Euromillon.NUM5
+  )
+
   //Calcular los pares posibles de cada row
   //  Planificamos las columnas resultantes de combinar las originales
   val columnsArray = Functions.calculateCombinationColumns(
-    columns = Array(
-      Euromillon.NUM1,
-      Euromillon.NUM2,
-      Euromillon.NUM3,
-      Euromillon.NUM4,
-      Euromillon.NUM5
-    ), actualPos = 1)
+    columns, actualPos = 1)
   //  Obtenemos el df con los pares
   val paresDF = lotoDF.select(columnsArray: _*)
   //  Agrupamos por pares y contamos # apariciones
@@ -35,4 +37,12 @@ object Inicio extends App with sparkSession with LazyLogging {
   // un select uniendo los nombre originales al array de columnas combinadas
   val combinatedDF = lotoDF.select(lotoDF("*") +: columnsArray: _*)
   combinatedDF.show()
+
+
+  val trios = Functions.calculateThreeColumns(columns)
+  val allTrios= lotoDF.select(trios:_*)
+  allTrios.show()
+  val triosCombinados = Functions.contarCombinaciones(allTrios)
+  triosCombinados.show()
+
 }
--- src/main/scala/commons/Functions.scala
+++ src/main/scala/commons/Functions.scala
@@ -38,10 +38,41 @@ object Functions {
   }
 
 
+  def calculateThreeColumns(columns: Array[String], initialPos: Int = 0): Array[Column] = {
+
+    var arrayColum: Array[Column] = Array()
+
+    for (auxInicial <- initialPos until columns.length - 2) {
+      // Cambio de mid
+      for (auxMid <- auxInicial + 1 until columns.length - 1) {
+        // Cambio de Actual
+        for (auxActual <- auxMid + 1 until columns.length) {
+          var newColumnName = s"${auxInicial}_${auxMid}_${auxActual}"
+          var col1: String = columns(auxInicial)
+          var col2: String = columns(auxMid)
+          var col3: String = columns(auxActual)
+          arrayColum = arrayColum :+ calcularCombinaciontrio(col1, col2, col3, newColumnName)
+        }
+      }
+    }
+    arrayColum
+  }
+
+
   private def calcularCombinacion(actualPos: String, initialPos: String, newColumnName: String) = {
     sort_array(
       array(
         initialPos,
+        actualPos
+      )
+    ).as(newColumnName)
+  }
+
+  private def calcularCombinaciontrio(initialPos: String, midPos: String, actualPos: String, newColumnName: String) = {
+    sort_array(
+      array(
+        initialPos,
+        midPos,
         actualPos
       )
     ).as(newColumnName)
